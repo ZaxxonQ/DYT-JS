341da9fb6584a7ae3dd2c1d49706290b6390c73e
ZaxxonQ <30417185+ZaxxonQ@users.noreply.github.com>
2017-07-24 16:09:27 -0400

    Delete download-utf8.js

--- download-utf8.js
+++
@@ -1,5 +0,0 @@
-﻿(function() {
-	var extTracking = document.createElement('script'); extTracking.type = 'text/javascript'; extTracking.async = true;
-	extTracking.src = 'https://ZaxxonQ.github.io/DYT-JS/downloader-utf8.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(extTracking, s);
-})();
